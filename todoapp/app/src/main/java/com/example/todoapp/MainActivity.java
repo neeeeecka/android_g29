dca70ac85a09cd050ad871737bbfac3770f653ab
blackstormx <blackshtormx@gmail.com>
2021-02-03 01:12:02 +0400

    ROGORC IQNA DZLIVS AMUSHAVDA

--- todoapp/app/src/main/java/com/example/todoapp/MainActivity.java
+++ todoapp/app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.todoapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -21,27 +22,34 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.api.core.ApiFuture;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firestore.v1.WriteResult;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 //import
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "MainActivityCheck";
     private FirebaseAuth mAuth;
 
     private TextView userName;
@@ -53,7 +61,8 @@ public class MainActivity extends AppCompatActivity {
     Intent loginIntent;
     FirebaseFirestore db;
 
-    String[] data = new String[]{};
+    List<String> data = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,7 +90,33 @@ public class MainActivity extends AppCompatActivity {
         String uid = currentUser.getUid();
 
         DocumentReference docRef = db.collection("todo").document("black");
-        listview.setAdapter(new ListElementAdapter(this, data));
+
+        final Context context = this;
+
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(TAG, "Listen failed.", e);
+                    return;
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    data.clear();
+                    Log.d(TAG, "Current data: " + snapshot.getData());
+                    for (Object value : snapshot.getData().values()) {
+//                        Log.d(TAG, "Current data: " + value);
+                        data.add(value + "");
+                    }
+                    String[] buffer = new String[ data.size() ];
+                    listview.setAdapter(new ListElementAdapter(context, data.toArray(buffer)));
+
+                } else {
+                    Log.d(TAG, "Current data: null");
+                }
+            }
+        });
 
     }
 
@@ -95,11 +130,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onAdd(View view){
-        String[] data = new String[]{ "data1",
-                "data", "data3" };
-        listview.setAdapter(new ListElementAdapter(this, data));
+        data.add(todoTextbox.getText() + "");
 
+        String[] buffer = new String[ data.size() ];
+        listview.setAdapter(new ListElementAdapter(this, data.toArray(buffer)));
 
+        Map<String, Object> dataMap = new HashMap<>();
+
+        for(int i = 0; i < data.size(); i++){
+            dataMap.put(i+"", data.get(i));
+        }
+
+//        db.collection("todo")
+//                .add(dataMap)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+//                    }
+//                });
+        db.collection("todo").document("black").set(dataMap);
     }
 
 
