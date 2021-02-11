bcc993e1e708f0b1dc7975f65eb242c630ab97e5
blackstormx <blackshtormx@gmail.com>
2021-02-11 21:06:04 +0400

    design

---
+++ todoappKotlin/app/src/main/java/com/example/todoappkotlin/LoginActivity.kt
@@ -0,0 +1,96 @@
+package com.example.todoappkotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private var RC_SIGN_IN = 123
+    private var TAG = "LoginActivity"
+    private lateinit var signInButton: SignInButton
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        signInButton = findViewById(R.id.sign_in_button);
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val client = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
+
+        val currentUser = mAuth.currentUser
+
+        if (currentUser == null) {
+            signInButton.setOnClickListener{
+                val signInIntent = client.signInIntent
+                startActivityForResult(signInIntent, RC_SIGN_IN)
+            };
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // ...
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = mAuth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    // ...
+                    updateUI(null)
+                }
+
+                // ...
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user == null) {
+            Log.d(TAG, "Go back")
+        } else {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
--- todoappKotlin/app/src/main/java/com/example/todoappkotlin/MainActivity.kt
+++ todoappKotlin/app/src/main/java/com/example/todoappkotlin/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.todoappkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.SignInButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private var TAG = "MainActivity"
+    private lateinit var signInButton: SignInButton
+    private lateinit var mAuth: FirebaseAuth
+//    private lateinit var currentUser: FirebaseUser
+
+    private lateinit var userName: TextView
+    private lateinit var logout: Button
+    private lateinit var todoTextbox: EditText
+    private lateinit var add: Button
+    private lateinit var listview: ListView
+    private lateinit var loginIntent: Intent
+
+    private lateinit var db: FirebaseFirestore
+
+    private var data: List<String> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        userName = findViewById(R.id.userName);
+        logout = findViewById(R.id.logout);
+        todoTextbox = findViewById(R.id.todoTextbox);
+        add = findViewById(R.id.add);
+        listview = findViewById(R.id.listView);
+
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth.currentUser
+        loginIntent = Intent(this, LoginActivity::class.java)
+
+        if(currentUser == null) {
+            startActivity(loginIntent);
+        }
+
+    }
+    fun onAdd(view: View){
+
+    }
+    fun onLogOut(view: View){
+
     }
 }
